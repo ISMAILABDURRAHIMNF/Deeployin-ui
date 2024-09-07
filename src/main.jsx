5f258a264d9dfcbe009a553a7a65059bf6160143
Ismail Abdurrahim <isma21173ti@student.nurulfikri.ac.id>
2024-09-07 21:07:03 +0700

    Register & Home Upload Page

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,26 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Login from './Login'
+import Register from './Register'
+import Home from './Home'
 import './index.css'
+
+export default function Main() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Login />
+    <Main />
   </StrictMode>,
 )
